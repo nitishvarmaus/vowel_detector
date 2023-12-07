394453d2c71fbce3fc0a1210baea492da71d67c8
nitishvarmaus <61003268+nitishvarmaus@users.noreply.github.com>
2023-12-07 06:25:04 +0530

    Create test2.jsx

---
+++ test2.jsx
@@ -0,0 +1,7 @@
+// This is a simple JavaScript code for testing
+function greet(name) {
+  console.log(`Hello, ${name}! Welcome to the world of JavaScript.`);
+}
+
+// Test the greet function
+greet("John");
